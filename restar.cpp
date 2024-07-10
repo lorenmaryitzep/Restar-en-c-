71a75ea086ca29f01e1e46ce8be40d406006f76f
lorenmaryitzep <raquelita682007@gmail.com>
2024-07-10 11:02:33 -0600

    Add files via upload

---
+++ restar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int valor1, valor2, respuesta;
+cout<<"Operaciones básicas"<<endl;
+cout<<"Entrada y salidas"<<endl;
+cout<<"Ingrese el primer valor"<<endl;
+cin>>valor1;
+cout<<"Ingrese el segundo valor"<<endl;
+cin>>valor2;
+respuesta = valor1 - valor2;
+cout<<respuesta;
+
+return 0;
+}
